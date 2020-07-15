24ccb091882ef0c36b78b0fc5699ff16df9d0060
sajana96 <weerakonsajana@gmail.com>
2020-07-15 19:19:39 +0530

    Search for files in a file path

--- src/mediaconverter/MediaConverter.java
+++ src/mediaconverter/MediaConverter.java
@@ -5,6 +5,16 @@
  */
 package mediaconverter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
 /**
  *
  * @author Sajana
@@ -14,8 +24,32 @@ public class MediaConverter {
     /**
      * @param args the command line arguments
      */
+    
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+       
+        String dirName = "E:/Sajana/UOK/IPT/Media";
+        try {
+            
+//            Files.walk(Paths.get(dirName))
+//            .filter(Files::isDirectory)
+//            .forEach(System.out::println);
+            //Lis
+            List<File> filesInFolder = Files.walk(Paths.get(dirName))
+                                .filter(Files::isRegularFile)
+                                .map(Path::toFile)
+                                .collect(Collectors.toList());
+            for(File file:filesInFolder){
+            System.out.println(file);
+        }
+        } 
+        catch (Exception e) {
+            e.printStackTrace(); 
+              
+            // Prints what exception has been thrown 
+            System.out.println(e); 
+        }
     }
     
 }
